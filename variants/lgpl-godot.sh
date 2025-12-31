3164aacb18f0bbec5e174e8d101d9647f5484dd3
Taylor Finnell <tmfinnell@gmail.com>
2025-12-31 18:26:18 -0500

    Add Bink video/audio codec support for NovaLogic games

--- variants/lgpl-godot.sh
+++ variants/lgpl-godot.sh
@@ -4,5 +4,6 @@ VP9="--enable-demuxer=matroska --enable-decoder=vp9 --enable-parser=vp9"
 VP8="--enable-decoder=vp8 --enable-parser=vp8"
 H264="--enable-decoder=mpeg4,h264,aac,aac_latm,mp3 --enable-demuxer=mov,aac,flv,avi --enable-parser=h264,mpeg4video,mpegaudio,mpegvideo,aac,aac_latm"
 OPUS="--enable-libopus --enable-parser=opus --enable-decoder=opus --enable-decoder=libopus"
+BINK="--enable-decoder=binkvideo --enable-decoder=binkaudio_dct --enable-decoder=binkaudio_rdft --enable-demuxer=bink"
 
-FF_CONFIGURE="$FF_CONFIGURE $VORBIS $VP9 $VP8 $H264 $OPUS"
+FF_CONFIGURE="$FF_CONFIGURE $VORBIS $VP9 $VP8 $H264 $OPUS $BINK"
